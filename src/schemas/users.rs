8eef156025f83042ed89b1511eaf6a02e8e21cde
ATechnoHazard <amolele@gmail.com>
2020-09-14 23:30:57 +0530

    :fire: remove unused UserInDb schema
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- src/schemas/users.rs
+++ src/schemas/users.rs
@@ -12,28 +12,3 @@ pub struct UserUpdate {
     pub username: Option<String>,
     pub is_active: Option<bool>,
 }
-
-// #[derive(Serialize, Deserialize)]
-// pub struct UserInDb {
-//     pub id: Uuid,
-//     pub username: String,
-//     pub is_active: bool,
-// }
-//
-// impl UserInDb {
-//     pub fn to_model(&self) -> User {
-//         User {
-//             id: self.id,
-//             username: self.username.clone(),
-//             is_active: self.is_active,
-//         }
-//     }
-//
-//     pub fn from_model(user: &User) -> UserInDb {
-//         UserInDb {
-//             id: user.id,
-//             is_active: user.is_active,
-//             username: user.username.clone(),
-//         }
-//     }
-// }
